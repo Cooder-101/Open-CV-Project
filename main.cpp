fe3704252b4b6c9530f31fd78ddd696de1a60bc4
Anurag Kotwal <54587057+Cooder-101@users.noreply.github.com>
2021-01-13 00:34:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,34 +7,34 @@ using namespace cv;
 Mat rotateimg(Mat src,int angle){
     Point2f center((src.cols-1)/2.0, (src.rows-1)/2.0);
     Mat rot = getRotationMatrix2D(center, angle, 1.0);
-    Rect2f box = RotatedRect(cv::Point2f(), src.size(), angle).boundingRect2f();
-    rot.at<double>(0,2) += box.width/2.0 - src.cols/2.0;
-    rot.at<double>(1,2) += box.height/2.0 - src.rows/2.0;
+    Rect2f grid = RotatedRect(cv::Point2f(), src.size(), angle).boundingRect2f();
+    rot.at<double>(0,2) += grid.width/2.0 - src.cols/2.0;
+    rot.at<double>(1,2) += grid.height/2.0 - src.rows/2.0;
     Mat final_img;
-    warpAffine(src, final_img, rot, box.size());
+    warpAffine(src, final_img, rot, grid.size());
     return final_img;
 }
 
 int main() {
     cout<<"Enter Image File Path : ";
-    string imgpath;
-    getline(cin,imgpath);
-    Mat src = imread(imgpath,IMREAD_UNCHANGED);
+    string img;
+    getline(cin,img);
+    Mat src = imread(img,IMREAD_UNCHANGED);
     if(src.empty()){
-        cout<<"ERROR Loading Image File."<<endl;
+        cout<<"Image is not Loading"<<endl;
         return -1;
     }
     cout<<"Enter Angle : ";
     int angle;
     cin>>angle;
     angle = angle%360;
-    Mat rotatedimg = rotateimg(src,angle);
-    if(rotatedimg.empty()){
-        cout<<"ERROR Rotating Image."<<endl;
+    Mat rotimg = rotateimg(src,angle);
+    if(rotimg.empty()){
+        cout<<"Unable to rotate the image"<<endl;
         return -1;
     }
     imshow("Original Image", src);
-    imshow("Rotated Image", rotatedimg);
+    imshow("Rotated Image", rotimg);
     waitKey(0);
     return 0;
 }
